21c8aae70da4adc2e8115e75a2d13a6e4cb94e8c
antruc <admin@antruc.dev>
2026-07-17 12:37:57 -0600

    Add eslint-plugin-vue

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
 import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
 import globals from 'globals'
 import prettier from 'eslint-config-prettier'
 
 export default [
   js.configs.recommended,
+  // Lints the .vue SFCs (template + script); without this plugin they were
+  // silently skipped by eslint.
+  ...pluginVue.configs['flat/recommended'],
   prettier,
   {
     languageOptions: {
